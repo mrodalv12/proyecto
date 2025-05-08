855ece842b26f2ce0b8bb61668f648a82ab21cf4
RAAAmajo <mrodalv@upv.edu.es>
2025-05-08 17:22:53 +0200

    mitad de solicitudes terminada

--- src/app/PAS/Solicitudes_PAS.php
+++ src/app/PAS/Solicitudes_PAS.php
@@ -32,171 +32,195 @@ if ($thisUser->rol != "PAS") {
     <section class="contenido">
         <h1>Solicitudes</h1>
         <!--botones primarios-->
-        <ul class="bandeja_columna1">
-            <li><button onclick="mostrarContenido('de_entrada')">Bandeja de entrada</button></li>
-            <li><button onclick="mostrarContenido('hechas')">Solicitudes Hechas</button></li>
-            <li><button onclick="mostrarContenido('rechazadas')">Papelera</button></li>
-        </ul>
-        <!--fin de botones primarios-->
+        <div class="todas_las_solicitudes">
+            <div class="bandeja_columna1">
+                    <a onclick="activarEstilo(this); mostrarContenido('solicitud_pendiente_')">
+                            <p>Bandeja de entrada</p>
+                            <p>num</p>
+                            </a>
+                    <a onclick="activarEstilo(this); mostrarContenido('solicitud_hecha_')">
+                        <p>Bandeja de Hechas</p>
+                        <p>num</p>
+                    </a>
+                    <a onclick="activarEstilo(this); mostrarContenido('solicitud_rechazada_')">
+                        <p>Papelera</p>
+                        <p>num</p>
+                    </a>
+            </div>
+
+            <!--fin de botones primarios-->
+
+            <!--botones secundarios-->
+            <div class="columna_2">
+                <!--vacio-->
+                <p id="bandeja_vacia">vacio</p>
+                <!--fin de vacio-->
+                <!--bandeja de entrada-->
+                <div class="bandeja">
+                    <a id="solicitud_pendiente_contenido_1" class="item-columna2" onclick="hacerAlgo()">
+                            <ul>
+                                <li><p>Lorem ipsum movedae tiresio</p></li>
+                                <li><img src="https://placehold.co/30x30" alt="aceptada?"></li>
+                            </ul>
+                    </a>
+
+                    <a id="solicitud_pendiente_contenido_2" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                            <li><img src="https://placehold.co/30x30" alt="aceptada?"></li>
+                        </ul>
+                    </a>
+                    <a id="solicitud_pendiente_contenido_3" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                            <li><img src="https://placehold.co/30x30" alt="aceptada?"></li>
+                        </ul>
+                    </a>
+                    <a id="solicitud_pendiente_contenido_4" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                            <li><img src="https://placehold.co/30x30" alt="aceptada?"></li>
+                        </ul>
+                    </a>
+                    <a id="solicitud_pendiente_contenido_5" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                            <li><img src="https://placehold.co/30x30" alt="aceptada?"></li>
+                        </ul>
+                    </a>
+                    <a id="solicitud_pendiente_contenido_6" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                            <li><img src="https://placehold.co/30x30" alt="aceptada?"></li>
+                        </ul>
+                    </a>
+                </div>
 
 
+                <!--bandeja de hechas-->
+                <div class="bandeja">
+                    <a id="solicitud_hecha_contenido_7" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                        </ul>
+                    </a>
+                    <a id="solicitud_hecha_contenido_7" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                        </ul>
+                    </a>
+                </div>
 
 
-        <!--botones secundarios-->
-        <div class="columna_2">
-            <!--bandeja de entrada-->
-            <ol class="bandeja" id="de_entrada">
-                <li id="1"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                        <img src="https://placehold.co/30x30" alt="aceptada?">
-                    </button></li>
-                <li id="2"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                        <img src="https://placehold.co/30x30" alt="aceptada?">
-                    </button></li>
-                <li id="3"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                        <img src="https://placehold.co/30x30" alt="aceptada?">
-                    </button></li>
-                <li id="4"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                        <img src="https://placehold.co/30x30" alt="aceptada?">
-                    </button></li>
-                <li id="5"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                        <img src="https://placehold.co/30x30" alt="aceptada?">
-                    </button></li>
-                <li id="6"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                        <img src="https://placehold.co/30x30" alt="aceptada?">
-                    </button></li>
-            </ol>
+                <!--bandeja de rechazadas-->
+                <div class="bandeja">
+                    <a id="solicitud_rechazada_contenido_9" class="item-columna2" onclick="hacerAlgo()">
+                        <ul>
+                            <li><p>Lorem ipsum movedae tiresio</p></li>
+                        </ul>
+                    </a>
+                </div>
+            </div>
+            <!--fin de botones secundarios-->
 
+            <!--vista del contenido-->
+            <div class="solicitudes_vista_del_contenido">
 
-            <!--bandeja de hechas-->
-            <ol class="bandeja" id="hechas">
-                <li id="7"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                    </button></li>
-                <li id="8"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                    </button></li>
-            </ol>
+                <ul class="contenidoPendientes" id="contenido_1">
+                    <img src="https://placehold.co/50x50" alt="user">
+                    <div>
+                        <li>nombre y apellido</li>
+                        <li>rol</li>
+                        <li>DNI</li>
+                        <li>asunto</li>
+                        <li>tema</li>
+                        <li>descripcion</li>
+                        <div>
+                            <button>aceptada</button>
+                            <button>rechazada</button>
+                        </div>
+                    </div>
+                </ul>
+                <!--sin contenido seleccionado-->
+                <ul id="sin_contenido">
+                    <li><img src="https://placehold.co/100x100" alt="icono sin solicitudes"></li>
+                    <li><p>No hay solicitudes que atender</p></li>
+                </ul>
 
+                <!--contenido de solicitudes pendientes-->
 
-            <!--bandeja de rechazadas-->
-            <ol class="bandeja" id="rechazadas">
-                <li id="9"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                    </button></li>
-                <li id="10"><button>
-                        <p>Lorem ipsum movedae tiresio</p>
-                    </button></li>
-            </ol>
+                <ul class="contenidoPendientes" id="contenido_2">
+                    <img src="https://placehold.co/50x50" alt="user">
+                    <li>nombre y apellido</li>
+                    <li>rol</li>
+                    <li>DNI</li>
+                    <li>asunto</li>
+                    <li>tema</li>
+                    <li>descripcion</li>
+                    <ul>
+                        <li><button>aceptada</button></li>
+                        <li><button>rechazada</button></li>
+                    </ul>
+                </ul>
+                <ul class="contenidoPendientes" id="contenido_3">
+                    <img src="https://placehold.co/50x50" alt="user">
+                    <li>nombre y apellido</li>
+                    <li>rol</li>
+                    <li>DNI</li>
+                    <li>asunto</li>
+                    <li>tema</li>
+                    <li>descripcion</li>
+                    <ul>
+                        <li><button>aceptada</button></li>
+                        <li><button>rechazada</button></li>
+                    </ul>
+                </ul>
+                <ul class="contenidoPendientes" id="contenido_4">
+                    <img src="https://placehold.co/50x50" alt="user">
+                    <li>nombre y apellido</li>
+                    <li>rol</li>
+                    <li>DNI</li>
+                    <li>asunto</li>
+                    <li>tema</li>
+                    <li>descripcion</li>
+                    <ul>
+                        <li><button>aceptada</button></li>
+                        <li><button>rechazada</button></li>
+                    </ul>
+                </ul>
+                <ul class="contenidoPendientes" id="contenido_5">
+                    <img src="https://placehold.co/50x50" alt="user">
+                    <li>nombre y apellido</li>
+                    <li>rol</li>
+                    <li>DNI</li>
+                    <li>asunto</li>
+                    <li>tema</li>
+                    <li>descripcion</li>
+                    <ul>
+                        <li><button>aceptada</button></li>
+                        <li><button>rechazada</button></li>
+                    </ul>
+                </ul>
+                <ul class="contenidoPendientes" id="contenido_6">
+                    <img src="https://placehold.co/50x50" alt="user">
+                    <li>nombre y apellido</li>
+                    <li>rol</li>
+                    <li>DNI</li>
+                    <li>asunto</li>
+                    <li>tema</li>
+                    <li>descripcion</li>
+                    <ul>
+                        <li><button>aceptada</button></li>
+                        <li><button>rechazada</button></li>
+                    </ul>
+                </ul>
+
+            </div>
         </div>
-        <!--fin de botones secundarios-->
 
-
-
-
-
-        <!--vista del contenido-->
-        <div class="solicitudes_vista_del_contenido">
-
-
-            <!--sin contenido seleccionado-->
-            <ul id="sin_contenido">
-                <li><img src="https://placehold.co/100x100" alt="icono sin solicitudes"></li>
-                <li><p>No hay solicitudes que atender</p></li>
-            </ul>
-
-            <!--contenido de solicitudes pendientes-->
-            <ul id="contenido_1">
-                <img src="https://placehold.co/50x50" alt="user">
-                <li>nombre y apellido</li>
-                <li>rol</li>
-                <li>DNI</li>
-                <li>asunto</li>
-                <li>tema</li>
-                <li>descripcion</li>
-                <ul>
-                    <li><button>aceptada</button></li>
-                    <li><button>rechazada</button></li>
-                </ul>
-            </ul>
-            <ul id="contenido_2">
-                <img src="https://placehold.co/50x50" alt="user">
-                <li>nombre y apellido</li>
-                <li>rol</li>
-                <li>DNI</li>
-                <li>asunto</li>
-                <li>tema</li>
-                <li>descripcion</li>
-                <ul>
-                    <li><button>aceptada</button></li>
-                    <li><button>rechazada</button></li>
-                </ul>
-            </ul>
-            <ul id="contenido_3">
-                <img src="https://placehold.co/50x50" alt="user">
-                <li>nombre y apellido</li>
-                <li>rol</li>
-                <li>DNI</li>
-                <li>asunto</li>
-                <li>tema</li>
-                <li>descripcion</li>
-                <ul>
-                    <li><button>aceptada</button></li>
-                    <li><button>rechazada</button></li>
-                </ul>
-            </ul>
-            <ul id="contenido_4">
-                <img src="https://placehold.co/50x50" alt="user">
-                <li>nombre y apellido</li>
-                <li>rol</li>
-                <li>DNI</li>
-                <li>asunto</li>
-                <li>tema</li>
-                <li>descripcion</li>
-                <ul>
-                    <li><button>aceptada</button></li>
-                    <li><button>rechazada</button></li>
-                </ul>
-            </ul>
-            <ul id="contenido_5">
-                <img src="https://placehold.co/50x50" alt="user">
-                <li>nombre y apellido</li>
-                <li>rol</li>
-                <li>DNI</li>
-                <li>asunto</li>
-                <li>tema</li>
-                <li>descripcion</li>
-                <ul>
-                    <li><button>aceptada</button></li>
-                    <li><button>rechazada</button></li>
-                </ul>
-            </ul>
-            <ul id="contenido_6">
-                <img src="https://placehold.co/50x50" alt="user">
-                <li>nombre y apellido</li>
-                <li>rol</li>
-                <li>DNI</li>
-                <li>asunto</li>
-                <li>tema</li>
-                <li>descripcion</li>
-                <ul>
-                    <li><button>aceptada</button></li>
-                    <li><button>rechazada</button></li>
-                </ul>
-            </ul>
-
-        </div>
 
         <!--fin de vista de contenido-->
     </section>
-
-
-
 
 
     <!----------------------------------------------------------------------------------------------------------->
--- src/js/solicitudes_PAS.js
+++ src/js/solicitudes_PAS.js
@@ -1,8 +1,26 @@
 function mostrarContenido(id) {
-    // Oculta todos los contenidos primero
-    document.querySelectorAll('.bandeja').forEach(ol => {
-        ol.classList.remove('activo');
+    document.querySelectorAll('[id^="solicitud_"]').forEach(solicitud => {
+        solicitud.classList.remove('activo');
     });
-    // Muestra solo el que fue seleccionado
-    document.getElementById(id).classList.add('activo');
+    let hayElementos = false;
+
+    const elementos = document.querySelectorAll(`[id^="${id}"]`);
+    elementos.forEach(elemento => {
+        elemento.classList.add('activo');
+        hayElementos = true;
+        console.log("Activado: ", elemento.id);
+    });
+    const mensajeVacio = document.getElementById("bandeja_vacia");
+    if (mensajeVacio) {
+        mensajeVacio.style.display = hayElementos ? "none" : "block";
+    }
+}
+//---------------------------------------------------------------------------------
+function activarEstilo(elemento) {
+    // Quitar la clase 'activo' de todos los enlaces
+    document.querySelectorAll('.bandeja_columna1 a').forEach(a => {
+        a.classList.remove('activo');
+    });
+    // Agregar clase al que fue clickeado
+    elemento.classList.add('activo');
 }
