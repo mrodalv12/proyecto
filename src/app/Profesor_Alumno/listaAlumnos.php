fa01682f66a410d17d7b7d4db25fbe88273a5abc
RAAAmajo <mrodalv@upv.edu.es>
2025-05-16 02:56:41 +0200

    ahora si creo

---
+++ src/app/Profesor_Alumno/listaAlumnos.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Entregar Tareas</title>
+    <link rel="preload" href="../../css/headeryfooterPROA.css" as="style" />
+    <link rel="stylesheet" href="../../css/headeryfooterPROA.css" />
+    <link rel="stylesheet" href="../../css/listaAlumnos.css" />
+    <link rel="stylesheet" href="../../css/MOVILmenu-Profesores_y_alumnos.css">
+</head>
+<body>
+
+<!-- Header -->
+<?php include "../includes/header_proa.php" ?>
+
+<main>
+    <section class="tablaAlumnos">
+        <h1><a href="tareasProfesor.php">TAREAS</a></h1>
+    <h2>Tareas entregadas</h2>
+
+    <table>
+        <thead>
+        <tr>
+            <th>Alumnos</th>
+            <th>Calificación</th>
+            <th>Estado</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Alumno 1</td>
+            <td>7/10</td>
+            <td class="tarea-entregada"><a href="tareasContenidoProfesor.php">Entregado</a></td>
+        </tr>
+        <tr>
+            <td>Alumno 2</td>
+            <td>-/10</td>
+            <td class="no-entregado">No entregado</td>
+        </tr>
+        <tr>
+            <td>Alumno 3</td>
+            <td>-/10</td>
+            <td class="tarea-entregada"><a href="tareasContenidoProfesor.php">Entregado</a></td>
+        </tr>
+        <tr>
+            <td>Alumno 4</td>
+            <td>-/10</td>
+            <td class="tarea-entregada"><a href="tareasContenidoProfesor.php">Entregado</a></td>
+        </tr>
+        <tr>
+            <td>Alumno 5</td>
+            <td>-/10</td>
+            <td class="tarea-entregada"><a href="tareasContenidoProfesor.php">Entregado</a></td>
+        </tr>
+        <tr>
+            <td>Alumno 6</td>
+            <td>-/10</td>
+            <td class="tarea-entregada"><a href="tareasContenidoProfesor.php">Entregado</a></td>
+        </tr>
+        </tbody>
+    </table>
+    </section>
+</main>
+
+<?php include "../includes/footer_proa.php" ?>
+
+
+</body>
+</html>
---
+++ src/app/Profesor_Alumno/tareasContenido.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Entregar Tareas</title>
+    <link rel="preload" href="../../css/headeryfooterPROA.css" as="style" />
+    <link rel="stylesheet" href="../../css/headeryfooterPROA.css" />
+    <link rel="stylesheet" href="../../css/tareasContenido.css" />
+    <link rel="stylesheet" href="../../css/MOVILmenu-Profesores_y_alumnos.css" />
+</head>
+<body>
+
+<!-- Header -->
+<header>
+<?php include "../includes/header_proa.php" ?>
+</header>
+
+<div class="container">
+    <h1><a href="tareasAlumno.php">TAREAS</a></h1>
+    <h2>Título de la tarea</h2>
+
+    <div class="info-box">
+        <p>Fecha de entrega: 5 junio 2025. 23:55</p>
+        <p>Descripción de la tarea: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Número de reenvíos: 1</p>
+        <p>Estado: No empezado</p>
+        <p>Calificación: -/10</p>
+    </div>
+
+    <div class="instrucciones">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+    <div class="archivo-section">
+        <div class="upload-decorator" onclick="document.getElementById('archivo-upload').click()">
+            Haz clic o arrastra archivos aquí
+
+            <input type="file" id="archivo-upload" multiple hidden />
+        </div>
+
+        <div id="archivo-display" class="archivo-display"></div>
+        <p id="contador-archivos" class="contador-archivos"></p>
+
+        <button class="btn-enviar" onclick="enviarArchivos()">Enviar</button>
+        <p id="mensaje-enviado" class="mensaje-enviado">¡Archivos enviados!</p>
+        <p id="mensaje-error" class="mensaje-error">
+            Debes seleccionar al menos un archivo antes de enviar.
+        </p>
+
+    </div>
+
+</div>
+
+<!-- Footer -->
+<?php include "../includes/footer_proa.php" ?>
+
+<script src="../../js/tareasContenido.js"></script>
+</body>
+</html>
